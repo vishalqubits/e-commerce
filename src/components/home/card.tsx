2b0ca7e13dc0652bb2acee723d4ddced5cce373f
vishalqubits <vishalqubits@gmail.com>
2024-07-10 16:15:56 +0545

    add pagination

--- src/components/home/card.tsx
+++ src/components/home/card.tsx
@@ -1,4 +1,4 @@
-import { Rating } from "flowbite-react";
+import { Pagination, Rating } from "flowbite-react";
 import { useEffect, useState } from "react";
 
 type TCard = {
@@ -15,66 +15,92 @@ type TCard = {
 
 const Card = () => {
   const [products, setProducts] = useState([]);
-
-  const fetchData = async () => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    if (!response.ok) {
-      console.log("Unable to fetch products");
-      return;
-    }
-
-    const data = await response.json();
-
-    setProducts(data);
-  };
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPage = 8;
 
   useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        console.log("Unable to fetch products");
+        return;
+      }
+
+      const data = await response.json();
+
+      setProducts(data);
+    };
     fetchData();
-  });
+  }, []);
 
   if (!products.length) {
     console.log("No products found");
     return;
   }
+
+  const totalPages = Math.ceil(products.length / perPage);
+  const onPageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const startIndex = (currentPage - 1) * perPage;
+  const paginatedProducts = products.slice(startIndex, startIndex + perPage);
+
   return (
-    <div className="page-padding my-12 grid grid-cols-4 gap-6">
-      {products.map((item: TCard) => {
-        return (
-          <div
-            key={item.id}
-            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
-          >
-            <a href="#">
-              <div className="flex justify-center p-6">
-                <img className="rounded-lg h-48 " src={item.image} alt="" />
-              </div>
-            </a>
-            <div className="p-5">
+    <>
+      <div className="page-padding my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {paginatedProducts.map((item: TCard) => {
+          return (
+            <div
+              key={item.id}
+              className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
+            >
               <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.title}
-                </h5>
+                <div className="flex justify-center p-6">
+                  <img
+                    className="rounded-lg h-48 "
+                    src={item.image}
+                    alt="product"
+                  />
+                </div>
               </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {item.description}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {item.category}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                ${item.price}
-              </p>
-              <Rating>
-                <Rating.Star />
-                <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
-                  {item.rating.rate}
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-text2 dark:text-white">
+                    {item.title}
+                  </h5>
+                </a>
+                <p className="mb-3 font-normal text-text1 dark:text-gray-400 h-24 overflow-auto">
+                  {item.description}
                 </p>
-              </Rating>
+                <p className="mb-3 font-medium text-text2 uppercase dark:text-gray-400">
+                  {item.category}
+                </p>
+                <p className="mb-3 text-lg font-bold text-text2 dark:text-gray-400">
+                  ${item.price}
+                </p>
+                <Rating>
+                  <Rating.Star />
+                  <p className="ml-2 text-sm font-bold text-text2 dark:text-white">
+                    {item.rating.rate}
+                  </p>
+                </Rating>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+
+      <div className="flex overflow-x-auto sm:justify-center mb-6">
+        <Pagination
+          layout="navigation"
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={onPageChange}
+          showIcons
+        />
+      </div>
+    </>
   );
 };
 
